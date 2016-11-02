7b7168584f83aa6ce528463cd7167e94e0e88e29
woosheep <woosheep@qq.com>
2016-11-02 16:33:13 +0800

    upd

--- soweibo.py
+++ soweibo.py
@@ -13,13 +13,15 @@ def fetchweibos(kword):
 	'SINAGLOBAL=1645489266019.149.1470792372411; SCF=AtZCZIRlC5wiArrVF9MWe7qvnErH-dD5L4oSB-Ep7jFwx9YS5BE0oAOhwkkQFLWS7CSCkqI9QROmvcfU0d5uthU.; SUHB=079ygO35U-w-mV; UOR=,,login.sina.com.cn; ALF=1479894076; SUB=_2A251CadsDeTxGedJ41US-CfMzjmIHXVW9ckkrDV8PUJbkNAKLXPykW1xh9c7XwWqNxtK9m86_dVc8YfCCw..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWDJnyk.zvzlVgr7aS-ogCG5JpX5oz75NHD95QpS0nNe0n4eh-fWs4Dqcj.i--ciKLhi-2Ri--Ni-i8iKyWi--fiK.7iKyhi--fi-82i-2c; wvr=6; SWB=usrmdinst_12; _s_tentry=-; Apache=8448649781523.334.1477528061108; ULV=1477528061137:116:30:12:8448649781523.334.1477528061108:1477439907052; NSC_wjq_txfjcp_mjotij=ffffffff094113d445525d5f4f58455e445a4a423660; ULOGIN_IMG=14775291383503; WBStorage=86fb700cbf513258|undefined'}
 	#add %25
 	url = url %(quote(kword).replace('%', "%25"), time.strftime('%Y-%m-%d', time.localtime()))
-	try:
-		html = requests.get(url, headers=headers).content
-	except Exception, e:
-		#skip con err
-		print "Err",
-		time.sleep(5)
-		html = requests.get(url, headers=headers).content
+	while True:
+		try:
+			html = requests.get(url, headers=headers).content
+			break
+		except Exception, e:
+			#skip con err
+			print "..X",
+			time.sleep(5)
+			continue
 	return html, url
 
 def checkupd(html, url):
@@ -30,17 +32,18 @@ def checkupd(html, url):
 	if html.find("yzm_change") > -1:
 		print 'Blocked'
 		exit()
-	elif cur > kword_dict[kword]:
+	news = cur - kword_dict[kword]
+	if news >0:
 		kword_dict[kword] = cur
-		showmsg(url)
+		showmsg(url, news)
 	else:
 		print
 		
 
-def showmsg(msg):
-	print 'Got'
+def showmsg(url, news):
+	print 'Got %d' %news
 	if not is1st:
-		webbrowser.open_new_tab(msg)
+		webbrowser.open_new_tab(url)
 
 
 #txt should be utf8 format
@@ -50,6 +53,7 @@ def fetchkword():
 		kword_dict[kword] = 0
 
 is1st = True
+curtime = -1
 fetchkword()
 while True:
 	for kword in kword_dict.keys():
